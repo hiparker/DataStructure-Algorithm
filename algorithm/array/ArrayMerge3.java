bd88d0fc03b48e9cdebd5f06c4e2000224f31a99
pengcheng.zhou <meet.parker@foxmail.com>
2023-07-26 17:20:53 +0800

    feat: 合并k个升序列表

---
+++ algorithm/array/ArrayMerge3.java
@@ -0,0 +1,60 @@
+package algorithm.array;
+
+/**
+ * 合并k个升序列表
+ * 注：血与泪的教训 当时怎么没做出来了呢？
+ *
+ * @author 周鹏程
+ * @date 2023/7/26 17:01
+ */
+public class ArrayMerge3 {
+	public static void main(String[] args) {
+
+		int[][] lists = {{1,4,5},{1,3,4},{2,6}};
+
+		int parentLen = 0;
+		int len = 0;
+		for (int[] ints : lists) {
+			parentLen++;
+			len += ints.length;
+		}
+
+		int[] indexArray = new int[parentLen];
+		for (int i = 0; i < parentLen; i++) {
+			indexArray[i] = 0;
+		}
+
+		int[] arr = new int[len];
+		int index = 0;
+		while (index < len){
+			int minIndex = -1;
+			int minMum = 0;
+			for (int i = 0; i < indexArray.length; i++) {
+				// 非法判断
+				if(indexArray[i] >= lists[i].length){
+					continue;
+				}
+
+				if(minIndex == -1){
+					minIndex = i;
+					minMum = lists[i][indexArray[i]];
+					continue;
+				}
+
+				int tmp = lists[i][indexArray[i]];
+				if(tmp < minMum){
+					minIndex = i;
+					minMum = tmp;
+				}
+			}
+
+			arr[index++] = lists[minIndex][indexArray[minIndex]];
+			indexArray[minIndex]++;
+		}
+
+		for (int i : arr) {
+			System.out.println(i);
+		}
+	}
+
+}
