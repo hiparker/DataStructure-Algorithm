31a00a1dfc297c3babcbcddc446f0a19f76c5c3f
pengcheng.zhou <meet.parker@foxmail.com>
2023-08-03 20:03:50 +0800

    feat: 打印字符串的 全部子序列

--- algorithm/DynamicPlanning/PrintStringAllSubsquences.java
+++ algorithm/DynamicPlanning/PrintStringAllSubsquences.java
@@ -1,11 +1,12 @@
 package algorithm.DynamicPlanning;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 /**
- * 打印字符串的 全部子序列
- *
+ * 1. 打印字符串的 全部子序列
+ * 2. 打印字符串的 全部子序列, 要求不重复
  * @author 周鹏程
  * @date 2023/8/3 19:44
  */
@@ -13,30 +14,59 @@ public class PrintStringAllSubsquences {
 
 	public static void main(String[] args) {
 		String str = "abcd";
-		List<String> ans = printAllSubsquences(str);
+		List<String> ans = subs1(str);
 		for (String an : ans) {
+			System.out.println(an);
+		}
+
+		System.out.println("----------------------");
+
+		str = "aaa";
+		HashSet<String> ans2 = subs2(str);
+		for (String an : ans2) {
 			System.out.println(an);
 		}
 	}
 
-	public static List<String> printAllSubsquences(String str){
+	public static List<String> subs1(String str){
 		char[] strChars = str.toCharArray();
 		List<String> ans = new ArrayList<>();
-		process(strChars, 0, ans, "");
+		process1(strChars, 0, ans, "");
 		return ans;
 	}
 
-	private static void process(char[] strChars, int index, List<String> ans, String path){
+	public static HashSet<String> subs2(String str){
+		char[] strChars = str.toCharArray();
+		HashSet<String> ans = new HashSet<>();
+		process2(strChars, 0, ans, "");
+		return ans;
+	}
+
+	private static void process1(char[] strChars, int index, List<String> ans, String path){
 		if(index == strChars.length){
 			ans.add(path);
 			return;
 		}
 
 		String no = path;
-		process(strChars, index+1, ans, no);
+		process1(strChars, index+1, ans, no);
 
 		String yes = path + strChars[index];
-		process(strChars, index+1, ans, yes);
+		process1(strChars, index+1, ans, yes);
+	}
+
+
+	private static void process2(char[] strChars, int index, HashSet<String> ans, String path){
+		if(index == strChars.length){
+			ans.add(path);
+			return;
+		}
+
+		String no = path;
+		process2(strChars, index+1, ans, no);
+
+		String yes = path + strChars[index];
+		process2(strChars, index+1, ans, yes);
 	}
 
 }
